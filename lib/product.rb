e39609f7fb4ad28c53dad30e6d9363a10034de2e
John Kim <abcdefghijohnk@gmail.com>
2020-07-08 10:51:55 -0600

    create product class

---
+++ lib/product.rb
@@ -0,0 +1,27 @@
+class Product
+  attr_reader :category, :name, :unit_price, :quantity
+
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity
+  end
+
+  def total_price
+    (quantity.to_f) * @unit_price
+  end
+
+  def hoard
+    @quantity = '0'
+
+  end
+
+  def is_hoarded?
+    if @quantity == '0'
+      true
+    else
+      false
+    end
+  end
+end
