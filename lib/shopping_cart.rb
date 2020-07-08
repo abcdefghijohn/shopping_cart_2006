145185aa895bc56a797654e4a9c774d0bd2dbb2b
John Kim <abcdefghijohnk@gmail.com>
2020-07-08 10:54:07 -0600

    create shopping cart class

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,21 @@
+class ShoppingCart
+  attr_reader :name, :capacity, :products
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @products = []
+  end
+
+  def capacity
+    @capacity.to_i
+  end
+
+  def add_product(product)
+    @products.push(product)
+  end
+
+  def details
+    {name: @name, capacity: @capacity}
+  end
+end
